3d9281c34be363d4150acc7beb648b23f16d45f6
choegyeongmin <choegyeongmin@choegyeminuimbp>
2022-12-02 22:22:37 +0900

    initiol commit

---
+++ src/defaul/HelloWorld.java
@@ -0,0 +1,9 @@
+package defaul;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author choegyeongmin
+ *
+ */
+module HelloWorld {
+}
