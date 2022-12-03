c1567dff675336cf91df920770b38274769d8694
choegyeongmin <choegyeongmin@218.38.137.27>
2022-12-03 18:02:50 +0900

    자바의 변수 소개 및 연습문제

--- src/HelloWorld/HelloWorld.java
+++ src/HelloWorld/HelloWorld.java
@@ -7,9 +7,36 @@ public class HelloWorld {
 		System.out.println("Hi World");
 		System.out.printf("print %s", "Test" ).println();
 		
-	int i = 2;
+		System.out.printf("%d * %d = %d", 5, 3, 5 * 3).println();
 		
-	System.out.printf("%d * %d = %d", 5, i, 5 * i).println();
-	}
+		int a = 10;
+		int b = 20;
+		int c = 30;
+		
+		System.out.printf("%d + %d +%d = %d", a, b, c, a+b+c).println();
+		
+		a = 50;
+		
+		System.out.printf("%d + %d +%d = %d", a, b, c, a+b+c).println();
+		
+		b = 60;
+		
+		System.out.printf("%d + %d +%d = %d", a, b, c, a+b+c).println();
+		
+		c = 70;
+		
+		System.out.printf("%d + %d +%d = %d", a, b, c, a+b+c).println();
+		
+		int newVariable;
+		
+		int undeclaredVariable;
+		
+		int number = 5;
+		int number2 = number;	
+		
+		
 
+		
+		
+	}
 }
